d62272f54d805059477ab946301400c06f91e373
Jessie <jessiebesset@hotmail.fr>
2025-12-18 17:26:22 +0100

    liste de courses

--- bases_python.py
+++ bases_python.py
@@ -1,3 +1,6 @@
+""" Calculatrice """
+
+"""
 a = b =""
 
 while not (a.isdigit() and b.isdigit()):
@@ -8,3 +11,51 @@ while not (a.isdigit() and b.isdigit()):
         print ("Veuillez entrer deux nombres valides")
 
 print(f"Le résultat de l'addition de {a} et {b} est {int(a) + int(b)}")
+
+"""
+
+""" Liste de courses """
+
+liste_course=[]
+
+choice = input (" Choisissez parmi les 5 options suivantes: " \
+"1 : Ajouter un élément à la liste" \
+"2 : Retirer un élément de la liste" \
+"3 : Afficher la liste" \
+"4 : Vider la liste" \
+"5 : Quitter" \
+"👉 Votre choix : ")
+
+while True : 
+    if choice == "1" :
+        ajout = input("Quel produit souhaitez-vous ajouter : ")
+        liste_course.append(ajout)
+        
+    elif choice == "2" : 
+        retrait = input ("Quel produit produit souhaitez-vous retirer : ")
+        if retrait in liste_course:
+            liste_course.remove(retrait)
+        else:
+            print("Ce produit n'est pas dans la liste")
+        
+    elif choice == "3" : 
+        print("Voici votre liste de course: ", liste_course)
+        
+    elif choice == "4" :
+        liste_course.clear()
+        print ("Liste de course vidée")
+        
+    elif choice == "5": 
+        print ("A bientôt")
+        break
+
+    else : 
+        print ("Votre choix ne correspond à aucune option")
+
+    choice = input (" Choisissez parmi les 5 options suivantes: " \
+    "1 : Ajouter un élément à la liste" \
+    "2 : Retirer un élément de la liste" \
+    "3 : Afficher la liste" \
+    "4 : Vider la liste" \
+    "5 : Quitter" \
+    "👉 Votre choix : ")
